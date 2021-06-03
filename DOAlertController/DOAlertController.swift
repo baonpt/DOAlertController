f29d3c06f6eac8e2b9c106cb2d9f2b6087d7c3e3
MacOS <090bravoiosdeveloper@gmail.com>
2021-06-04 02:54:38 +0700

    Updated

--- DOAlertController/DOAlertController.swift
+++ DOAlertController/DOAlertController.swift
@@ -9,6 +9,124 @@
 import Foundation
 import UIKit
 
+extension Notification.Name {
+  static var alertActionEnabledDidChangeNotification = Notification.Name(rawValue: "AlertActionEnabledDidChangeNotification")
+}
+
+enum AlertActionStyle: Int {
+ case `default`
+ case cancel
+ case destructive
+}
+
+enum AlertControllerStyle: Int {
+ case actionSheet
+ case alert
+}
+
+// MARK: AlertAction
+class AlertAction: NSObject {
+  var title: String
+  var style: AlertActionStyle
+  var handler: ((AlertAction?) -> Void)?
+  var enabled: Bool {
+    didSet {
+      if oldValue != enabled {
+        NotificationCenter.default.post(name: .alertActionEnabledDidChangeNotification, object: nil)
+      }
+    }
+  }
+
+  required init(title: String, style: AlertActionStyle, handler: ((AlertAction?) -> Void)?) {
+    self.title = title
+    self.style = style
+    self.handler = handler
+    self.enabled = true
+  }
+}
+
+// MARK: AlertAnimation
+class AlertAnimation: NSObject, UIViewControllerAnimatedTransitioning {
+
+  let isPresenting: Bool
+
+  init(isPresenting: Bool) {
+    self.isPresenting = isPresenting
+  }
+
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    if isPresenting {
+      return 0.45
+    } else {
+      return 0.25
+    }
+  }
+
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    if isPresenting {
+      self.presentAnimateTransition(transitionContext)
+    } else {
+      self.dismissAnimateTransition(transitionContext)
+    }
+  }
+
+  func presentAnimateTransition(_ transitionContext: UIViewControllerContextTransitioning) {
+    let alertController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
+    guard let alertController = alertController as? AlertController else { return }
+    let containerView = transitionContext.containerView
+
+    alertController.overlayView.alpha = 0.0
+    if alertController.isAlert() {
+      alertController.alertView.alpha = 0.0
+      alertController.alertView.center = alertController.view.center
+      alertController.alertView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+    } else {
+      alertController.alertView.transform = CGAffineTransform(translationX: 0,
+                                                              y: alertController.alertView.frame.height)
+    }
+    containerView.addSubview(alertController.view)
+
+    UIView.animate(withDuration: 0.25) {
+      alertController.overlayView.alpha = 1.0
+      if alertController.isAlert() {
+        alertController.alertView.alpha = 1.0
+        alertController.alertView.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
+      } else {
+        let bounce = alertController.alertView.frame.height / 480 * 10.0 + 10.0
+        alertController.alertView.transform = CGAffineTransform(translationX: 0, y: -bounce)
+      }
+    } completion: { finished in
+      UIView.animate(withDuration: 0.25) {
+        alertController.alertView.transform = CGAffineTransform.identity
+      } completion: { finished in
+        if finished {
+          transitionContext.completeTransition(true)
+        }
+      }
+    }
+  }
+
+  func dismissAnimateTransition(_ transitionContext: UIViewControllerContextTransitioning) {
+    let alertController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
+    guard let alertController = alertController as? AlertController
+    else { return }
+
+    UIView.animate(withDuration: self.transitionDuration(using: transitionContext)) {
+      alertController.overlayView.alpha = 0.0
+      if alertController.isAlert() {
+        alertController.alertView.alpha = 0.0
+        alertController.alertView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+      } else {
+        alertController.containerView.transform = CGAffineTransform(translationX: 0,
+                                                                    y: alertController.alertView.frame.height)
+      }
+    } completion: { _ in
+      transitionContext.completeTransition(true)
+    }
+  }
+}
+
+
 // MARK: AlertController Class
 class AlertController: UIViewController {
   
